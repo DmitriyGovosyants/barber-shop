e53b63fbc554db7278ca5e2ba38e6d2f4415140e
Yelyzaveta Potekhina <91630215+DmitriyGovosyants@users.noreply.github.com>
2022-04-12 13:04:54 +0300

    features - close mobile menu when click outside

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,10 +1,13 @@
 const mobileMenu = document.querySelector('.js-menu-container');
 const openMenuBtn = document.querySelector('.js-open-menu');
 const closeMenuBtn = document.querySelector('.js-close-menu');
-const mobileMenuCloseWhenAnchoring = document.querySelector('.js-mobile-menu-nav');
+const mobileMenuNav = document.querySelector('.js-mobile-menu-nav');
 
 openMenuBtn.addEventListener('click', toggleMenu);
 closeMenuBtn.addEventListener('click', toggleMenu);
+mobileMenuNav.addEventListener('click', mobileMenuCloseOnClickAnchor);
+document.addEventListener('click', closeMobileMenuOnClickOutside);
+window.matchMedia('(min-width: 768px)').addEventListener('change', mobileMenuCloseIfMediaTablet);
 
 function toggleMenu() {
   const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
@@ -17,14 +20,24 @@ function toggleMenu() {
   bodyScrollLock[scrollLockMethod](document.body);
 };
 
-mobileMenuCloseWhenAnchoring.addEventListener('click', e => {
+function mobileMenuCloseOnClickAnchor(e) {
   if (e.target.nodeName !== 'A') return;
   toggleMenu();
-});
+};
 
-window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+function closeMobileMenuOnClickOutside(e) {
+  const targetMobileMenu = e.target.closest('#mobile-menu');
+  const targetMobileMenuOpenBtn = e.target.classList[0] === 'js-svg-open-btn';
+  const mobileMenuIsOpen = document.querySelector('#mobile-menu.is-open');
+
+  if (!targetMobileMenu && !targetMobileMenuOpenBtn && mobileMenuIsOpen) {
+    toggleMenu();
+  }
+};
+
+function mobileMenuCloseIfMediaTablet(e) {
   if (!e.matches) return;
   mobileMenu.classList.remove('is-open');
   openMenuBtn.setAttribute('aria-expanded', false);
   bodyScrollLock.enableBodyScroll(document.body);
-});
+};
